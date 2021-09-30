875d015fc4743a457ca504ccf3ef3a9ae26d0eb3
n-peugnet <n.peugnet@free.fr>
2021-09-30 19:29:06 +0200

    simplify exp by not using mktemp

--- exp/exp.sh
+++ exp/exp.sh
@@ -2,12 +2,7 @@
 
 commits_file=$1
 repo_path=$2
-
-temp_templ="dna-backup-bench-XXXXX"
-mktemp="mktemp --tmpdir -d $temp_templ"
-
-temp=$($mktemp)
-echo temp dir: $temp >&2
+temp=$3
 
 cat $commits_file | while read i
 do
@@ -15,4 +10,3 @@ do
 	git -C $repo_path checkout $hash
 	../dna-backup commit -v 2 $repo_path $temp
 done
-du -bad 2 $temp
